e135eccf433551288cb100f394e3177253352da9
MikZarZSG <mikzarzsg@gmail.com>
2020-01-03 10:09:28 +0100

    Tworzenie formularza
    Nazwa
    Czy partner
    
    Klasy do organizacji inputow

--- klient_form.php
+++ klient_form.php
@@ -12,7 +12,20 @@
     </header>
     <main>
         <article>
-            <form action="nowy_klient.php" method="post"></form>
+            <form action="nowy_klient.php" method="post">
+                <div class="formRow">
+                    <label>Nazwa <input type="text" name="nazwa"></label>
+                </div>
+                
+                <div class="formRow">
+                    <fieldset>
+                        <legend>Czy jesteś partnerem</legend>
+                       
+                        <div><label>Tak<input type="radio" name="partner" value="tak"></label></div>
+                        <div><label>Nie<input type="radio" name="partner" value="nie" checked></label></div>
+                    </fieldset>
+                </div>
+            </form>
         </article>
     </main>
 </body>
